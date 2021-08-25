fcbc396d1e0690bd482cd58be629d3919625fc2e
José Trejo <joseatrejos>
2021-08-25 16:32:42 -0700

    Initial Commit

---
+++ pratica2Jesus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pratica2Jesus
+//
+//  Created by Alumno on 8/25/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
